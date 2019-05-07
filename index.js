a9631edd91f22b86ba81234bc1dbcd94d6000dad
Einar Persson <me@einarpersson.com>
2019-05-07 15:13:21 +0200

    Add support for public keys. Add tests

--- index.js
+++ index.js
@@ -14,9 +14,20 @@ function str2ab(str) {
 const buffer2base64uri = buff => buff.toString('base64').replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')
 
 const pem2jwk = (pem) => {
+  let kind = undefined
   // fetch the part of the PEM string between header and footer
-  const pemHeader = '-----BEGIN PRIVATE RSA KEY-----'
-  const pemFooter = '-----END PRIVATE RSA KEY-----'
+  const lines = pem.split('\n')
+  const pemHeader = lines[0]
+  const pemFooter = lines[lines.length - 1]
+
+  if (pemHeader === '-----BEGIN RSA PRIVATE KEY-----' && pemFooter === '-----END RSA PRIVATE KEY-----') {
+    kind = 'private'
+  } else if (pemHeader === '-----BEGIN RSA PUBLIC KEY-----' && pemFooter === '-----END RSA PUBLIC KEY-----') {
+    kind = 'public'
+  } else {
+    throw Error(`Headers not supported: ${pemHeader}\n ${pemFooter}`)
+  }
+
   const pemContents = pem.substring(pemHeader.length, pem.length - pemFooter.length)
 
   // base64 decode the string to get the binary data
@@ -26,15 +37,21 @@ const pem2jwk = (pem) => {
   const binaryDer = str2ab(binaryDerString)
   const sequence = asn.fromBER(binaryDer)
 
-  const fieldNames = [ 'n', 'e', 'd', 'p', 'q', 'dp', 'dq', 'qi' ]
+  const fieldNames = {
+    private: [ 'n', 'e', 'd', 'p', 'q', 'dp', 'dq', 'qi' ],
+    public: [ 'n', 'e' ]
+  }
 
-  const fields = sequence.result.valueBlock.value
-    .slice(1)
+  const fieldValues = kind === 'private'
+    ? sequence.result.valueBlock.value.slice(1)
+    : sequence.result.valueBlock.value
+
+  const fields = fieldValues
     .map(x => x.valueBlock.valueHex)
-    .map((val, i) => i === 1 || i === 2 ? val : val.slice(1))
+    .map((val , i) => i === 1 || val.byteLength % 2 === 0 ? val : val.slice(1))
     .map(x => Buffer.from(x))
     .map(b => buffer2base64uri(b))
-    .map((b64, i) => ({ [fieldNames[i]]: b64 }))
+    .map((b64, i) => ({ [fieldNames[kind][i]]: b64 }))
 
   return Object.assign({}, ...fields, { kty: 'RSA' })
 }
